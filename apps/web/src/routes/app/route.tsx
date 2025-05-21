7a6416e120241da239af2358baed4e9d96a93927
dorvillethiago <dorville.dev@gmail.com>
2025-05-20 23:51:03 -0300

    feat: add framer motion layout animations

--- apps/web/src/routes/app/route.tsx
+++ apps/web/src/routes/app/route.tsx
@@ -1,6 +1,7 @@
 import { ScreenLoader } from '@/components/shared/screen-loader'
 import { useAuth } from '@clerk/clerk-react'
 import { Navigate, Outlet, createFileRoute } from '@tanstack/react-router'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export const Route = createFileRoute('/app')({
 	component: LayoutComponent,
@@ -11,7 +12,21 @@ function LayoutComponent() {
 
 	return (
 		<>
-			{isLoaded && userId && <Outlet />}
+			<AnimatePresence mode="wait">
+				{isLoaded && userId && (
+					<motion.div
+						key="app-page"
+						initial={{ opacity: 0, y: 10 }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: -10 }}
+						transition={{ duration: 0.5 }}
+						className="size-full"
+					>
+						<Outlet />
+					</motion.div>
+				)}
+			</AnimatePresence>
+
 			{!isLoaded && <ScreenLoader />}
 			{isLoaded && !userId && <Navigate to="/auth/sign-in" />}
 		</>
--- apps/web/src/routes/auth/route.tsx
+++ apps/web/src/routes/auth/route.tsx
@@ -1,6 +1,7 @@
 import { ScreenLoader } from '@/components/shared/screen-loader'
 import { useAuth } from '@clerk/clerk-react'
 import { Navigate, Outlet, createFileRoute } from '@tanstack/react-router'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export const Route = createFileRoute('/auth')({
 	component: LayoutComponent,
@@ -12,7 +13,21 @@ function LayoutComponent() {
 	return (
 		<>
 			{isLoaded && userId && <Navigate to="/app/home" />}
-			{isLoaded && !userId && <Outlet />}
+
+			<AnimatePresence mode="wait">
+				{isLoaded && !userId && (
+					<motion.div
+						initial={{ opacity: 0, y: 10 }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: -10 }}
+						transition={{ duration: 0.5 }}
+						className="size-full"
+					>
+						<Outlet />
+					</motion.div>
+				)}
+			</AnimatePresence>
+
 			{!isLoaded && <ScreenLoader />}
 		</>
 	)
