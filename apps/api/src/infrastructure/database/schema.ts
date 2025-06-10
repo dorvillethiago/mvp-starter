2b8a75f67b85009494727083d3b53a7fc459359f
dorvillethiago <dorville.dev@gmail.com>
2025-06-10 11:23:26 -0300

    feat: add user schema to api

--- apps/api/src/infrastructure/database/schema.ts
+++ apps/api/src/infrastructure/database/schema.ts
@@ -1,8 +1,8 @@
-import { pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, varchar } from 'drizzle-orm/pg-core'
 
-export const usersTable = pgTable("users", {
-  id: varchar("id", { length: 255 }).primaryKey(),
-  clerkUserId: varchar("clerk_user_id", { length: 255 }).notNull().unique(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at").defaultNow().notNull(),
-});
+export const usersTable = pgTable('users', {
+	id: varchar('id', { length: 255 }).primaryKey(),
+	clerkUserId: varchar('clerk_user_id', { length: 255 }).notNull().unique(),
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	updatedAt: timestamp('updated_at').defaultNow().notNull(),
+})
