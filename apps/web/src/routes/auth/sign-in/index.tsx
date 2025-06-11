4531061bd89af0223c9d5613e8494f479390340a
dorvillethiago <dorville.dev@gmail.com>
2025-06-11 15:16:32 -0300

    fix: rollback auth sign-in screen file

--- apps/web/src/routes/auth/sign-in/index.tsx
+++ apps/web/src/routes/auth/sign-in/index.tsx
@@ -1,9 +1,56 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { UserRound } from 'lucide-react'
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+import { generateDocumentTitle } from '@/utils/generate-doc-title'
+import { Link, createFileRoute } from '@tanstack/react-router'
+import { LoginForm } from './-form'
 
 export const Route = createFileRoute('/auth/sign-in/')({
-	component: RouteComponent,
+	component: Login,
+	head: () => ({
+		meta: [{ title: generateDocumentTitle('Entrar') }],
+	}),
 })
 
-function RouteComponent() {
-	return <div>Hello "/auth/sign-in/"!</div>
+function Login() {
+	return (
+		<section className="relative grid h-full place-items-center py-32">
+			<div className="container mx-auto">
+				<div className="flex flex-col gap-4">
+					<Card className="mx-auto w-full max-w-md">
+						<CardHeader className="flex flex-col items-center">
+							<UserRound className="size-10 rounded-full bg-accent p-2.5 text-muted-foreground" />
+							<CardTitle className="text-xl">
+								Faça login com seu email
+							</CardTitle>
+							<CardDescription>
+								Digite suas informações para acessar
+							</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<LoginForm />
+						</CardContent>
+						<CardFooter>
+							<p className="mx-auto text-muted-foreground text-sm">
+								Não tem uma conta?{' '}
+								<Link
+									to="/auth/sign-up"
+									className="font-medium text-foreground hover:underline"
+								>
+									Crie uma agora
+								</Link>
+							</p>
+						</CardFooter>
+					</Card>
+				</div>
+			</div>
+		</section>
+	)
 }
