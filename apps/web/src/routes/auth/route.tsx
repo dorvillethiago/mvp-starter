a8c00ec46e031523cd2d330ff8a088ae119f3791
dorvillethiago <dorville.dev@gmail.com>
2025-07-04 15:24:29 -0300

    fix: add missing file

--- apps/web/src/routes/auth/route.tsx
+++ apps/web/src/routes/auth/route.tsx
@@ -1,9 +1,29 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { ScreenLoader } from '@/components/shared/screen-loader'
+import { FadeIn } from '@/lib/motion/fade-in'
+import { useAuth } from '@clerk/clerk-react'
+import { Navigate, Outlet, createFileRoute } from '@tanstack/react-router'
+import { AnimatePresence } from 'framer-motion'
 
 export const Route = createFileRoute('/auth')({
-	component: RouteComponent,
+	component: LayoutComponent,
 })
 
-function RouteComponent() {
-	return <div>Hello "/auth"!</div>
+function LayoutComponent() {
+	const { userId, isLoaded } = useAuth()
+
+	return (
+		<>
+			{isLoaded && userId && <Navigate to="/app/home" />}
+
+			<AnimatePresence mode="wait">
+				{isLoaded && !userId && (
+					<FadeIn className="size-full">
+						<Outlet />
+					</FadeIn>
+				)}
+			</AnimatePresence>
+
+			{!isLoaded && <ScreenLoader />}
+		</>
+	)
 }
